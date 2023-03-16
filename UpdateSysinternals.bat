dd76cd022f07a12ac344c168cd9d539b432eae27
maxrhys <66040395+maxrhys@users.noreply.github.com>
2023-03-16 23:45:33 +0000

    Update UpdateSysinternals.bat

--- UpdateSysinternals.bat
+++ UpdateSysinternals.bat
@@ -1,3 +1,6 @@
+:: Author: Rhys Wills
+:: Description: Downloads the Sysinternals Suite, extracts its contents, and deletes the downloaded zip file
+:: Notes: Run this script with an active internet connection.
 curl -o SysinternalsSuite.zip https://download.sysinternals.com/files/SysinternalsSuite.zip
 tar -xf SysinternalsSuite.zip
 del SysinternalsSuite.zip
